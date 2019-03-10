325aea04bb08e4005e66d72adda6d4336e33d75f
mariari <nymphet@protonmail.com>
2019-03-10 15:53:27 -0400

    to have name-spaces callable with a functor, it needs to be in an eval-when sadly

--- examples/applicative.lisp
+++ examples/applicative.lisp
@@ -14,54 +14,56 @@
     (fun bind f ms)
     (fun map  f ms)))
 
-;; currently don't have module includes... could easily derive it!
-(defmodule make ((mod *basic*)) ()
-  (defun return (ts)
-    (mod.return ts))
+(eval-when (:compile-toplevel :load-toplevel :execute)
 
-  (defun app (f-app app)
-    (mod.app f-app app))
+  ;; currently don't have module includes... could easily derive it!
+  (defmodule make ((mod *basic*)) ()
+    (defun return (ts)
+      (mod.return ts))
 
-  (defun derived-map (f ts)
-    (app (return f) ts))
+    (defun app (f-app app)
+      (mod.app f-app app))
 
-  (defun map (f xs)
-    (if (eq :custom (car mod.map))
-        (funcall (cadr mod.map) f xs)
-        (derived-map f xs)))
+    (defun derived-map (f ts)
+      (app (return f) ts))
 
-  (defun mapn (f arg1 &rest args)
-    (reduce (lambda (acc x) (app acc x)) args :initial-value (map f arg1))))
+    (defun map (f xs)
+      (if (eq :custom (car mod.map))
+          (funcall (cadr mod.map) f xs)
+          (derived-map f xs)))
 
-;; since we don't have nested modules yet, we don't give a signature for what we're given back!!
-(defmodule of-monad ((M *monad*)) ()
-  (let ((name
-         (defmodule struct *basic*
-           (defun return (x)
-             (M.return x))
+    (defun mapn (f arg1 &rest args)
+      (reduce (lambda (acc x) (app acc x)) args :initial-value (map f arg1))))
 
-           (defun app (f-app app)
-             (M.bind (lambda (f) (M.map f app)) f-app))
+  ;; since we don't have nested modules yet, we don't give a signature for what we're given back!!
+  (defmodule of-monad ((M *monad*)) ()
+    (let ((name
+           (defmodule struct *basic*
+             (defun return (x)
+               (M.return x))
 
-           (defparameter map (list :custom M.map)))))
-    ;; I shouldn't need to multiple-value-bind here
-    ;; it should already export to the right name-space
-    (multiple-value-bind (mod exps) (funcall make module:*functor-name* name)
-      ;; this even though it shows the correct data
-      ;; does not export anything to the right namespace...
-      ;; just odd, find fix, else I'll need to make an export macro
-      (export exps module:*functor-name*)
-      mod)))
+             (defun app (f-app app)
+               (M.bind (lambda (f) (M.map f app)) f-app))
 
-(defmodule list-monad struct *monad*
-  (defun map (f xs)
-    (mapcar f xs))
+             (defparameter map (list :custom M.map)))))
+      ;; I shouldn't need to multiple-value-bind here
+      ;; it should already export to the right name-space
+      (multiple-value-bind (mod exps) (funcall make module:*functor-name* name)
+        ;; this even though it shows the correct data
+        ;; does not export anything to the right namespace...
+        ;; just odd, find fix, else I'll need to make an export macro
+        (export exps module:*functor-name*)
+        mod)))
 
-  (defun bind (f xs)
-    (mapcan f xs))
+  (defmodule list-monad struct *monad*
+    (defun map (f xs)
+      (mapcar f xs))
 
-  (defun return (x) (list x)))
+    (defun bind (f xs)
+      (mapcan f xs))
 
+    (defun return (x) (list x)))
 
-(funcall of-monad 'list-app 'list-monad)
+  (funcall of-monad 'list-app 'list-monad))
+
 (print (list-app::return '3))

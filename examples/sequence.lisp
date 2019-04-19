c0de2ea53d5dfc8eaf394a2641b1f9e2a5d99716
Jacob Rosales Chase <coby@case.edu>
2019-04-19 19:41:34 -0400

    sequence progress

--- examples/sequence.lisp
+++ examples/sequence.lisp
@@ -5,7 +5,29 @@
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (defmodule step struct ()
-             (defclass t ()))
+             (defclass t ()
+               ;; need a sum type here, not sure how to do it
+               ))
   
   (defmodule expert struct ()
-             (defun next-step seq )))
+             (defun next-step (seq))
+
+             (defun delayed-fold-step (s &key init f finish)
+               (labels
+                   ((rec (s next finish f acc)
+                      (match-seq (funcall next s)
+                                 :done  (lambda ()    (funcall finish acc))
+                                 :skip  (lambda (s)   (funcall f acc () (lambda (x) (rec s next finish f x))))
+                                 :yeild (lambda (a s) (funcall f acc a  (lambda (x) (rec s next finish f x)))))))
+                 (rec (sequence-s s) (sequence-next s) finish f init)))))
+
+(defun unfold-step (init f)
+  (make-sequence :init init
+                 :f f))
+
+(defun unfold (init f)
+  (unfold-step init
+               (lambda (s) (let* ((res (funcall f s)))
+                        (if (null res)
+                            step.*done*
+                            (step.yeild RESUME-AT))))))
